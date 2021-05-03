4ada58d6124216e17f48c4d72ff61265a1b8325d
Matheus Franco <matheusefranco@gmail.com>
2021-05-03 09:12:47 -0300

    filas

---
+++ Modelo/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Modelo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSobre = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.listFila = new System.Windows.Forms.ListBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(63)))), ((int)(((byte)(159)))));
+            this.panel1.Controls.Add(this.btnSobre);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(141, 631);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnSobre
+            // 
+            this.btnSobre.FlatAppearance.BorderSize = 0;
+            this.btnSobre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSobre.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSobre.ForeColor = System.Drawing.Color.White;
+            this.btnSobre.Location = new System.Drawing.Point(0, 0);
+            this.btnSobre.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnSobre.Name = "btnSobre";
+            this.btnSobre.Size = new System.Drawing.Size(141, 90);
+            this.btnSobre.TabIndex = 9;
+            this.btnSobre.Text = "Sobre";
+            this.btnSobre.UseVisualStyleBackColor = true;
+            this.btnSobre.Click += new System.EventHandler(this.BtnSobre_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(0)))), ((int)(((byte)(238)))));
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.btnFechar);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(141, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(928, 154);
+            this.panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(474, 170);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 59);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Filas em C#";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 288);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 35);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(254, 280);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(284, 43);
+            this.txtNome.TabIndex = 5;
+            // 
+            // listFila
+            // 
+            this.listFila.FormattingEnabled = true;
+            this.listFila.ItemHeight = 35;
+            this.listFila.Location = new System.Drawing.Point(700, 280);
+            this.listFila.Name = "listFila";
+            this.listFila.Size = new System.Drawing.Size(263, 319);
+            this.listFila.TabIndex = 7;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.FlatAppearance.BorderSize = 0;
+            this.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemove.Image = global::Modelo.Properties.Resources.icon_remove;
+            this.btnRemove.Location = new System.Drawing.Point(379, 352);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(119, 81);
+            this.btnRemove.TabIndex = 8;
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatAppearance.BorderSize = 0;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Image = global::Modelo.Properties.Resources.icon;
+            this.btnAdd.Location = new System.Drawing.Point(254, 352);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(119, 81);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Modelo.Properties.Resources.sinfo;
+            this.pictureBox1.Location = new System.Drawing.Point(228, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(539, 181);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.FlatAppearance.BorderSize = 0;
+            this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFechar.Image = global::Modelo.Properties.Resources.close;
+            this.btnFechar.Location = new System.Drawing.Point(792, 6);
+            this.btnFechar.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(156, 109);
+            this.btnFechar.TabIndex = 12;
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.BtnFechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 35F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1069, 631);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.listFila);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSobre;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox listFila;
+        private System.Windows.Forms.Button btnRemove;
+    }
+}
+
---
+++ Modelo/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modelo
+{
+    public partial class Form1 : Form
+    {
+        Queue<String> fila = new Queue<String>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnSobre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void mostra()
+        {
+            listFila.Items.Clear();
+            foreach(String s in fila)
+            {
+                listFila.Items.Add(s);
+            }
+        }
+        //---------------
+        void limpa()
+        {
+            txtNome.Clear();
+            txtNome.Focus();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            fila.Enqueue(txtNome.Text);
+            mostra();
+            limpa();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (fila.Count > 0)
+            {
+                String s = fila.Dequeue();
+                MessageBox.Show(s + " foi atendido");
+            }
+            mostra();
+
+        }
+    }
+}
